44183307f5a209b672ade1cc78946dea71b4c7bf
Rafael A Goncalves <rafaelandradegs@gmail.com>
2022-06-23 08:54:54 -0300

    UUID

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,27 @@
-import React, { useReducer } from 'react'
-
+import React, { useState } from 'react'
+import { Nomes } from './Nomes'
 export function App() {
-  const [state, dispacth] = useReducer(reducer, {
-    player1: 0,
-    player2: 0
-  })
+  const [nome, setNome] = useState('')
+  const [nomes, setNomes] = useState([])
 
-  function reducer(state, action) {
-    switch (action.type) {
-      case 'SCORE1':
-        return {
-          player1: state.player1 + 1,
-          player2: state.player2
-        }
-      case 'SCORE2':
-        return {
-          player1: state.player1,
-          player2: state.player2 + 1
-        }
-
-      case 'RESETAR':
-        return {
-          player1: 0,
-          player2: 0
-        }
-
-      default:
-        return state
-    }
+  function guardarNome() {
+    setNomes([...nomes, nome])
+    setNome('')
   }
   return (
     <>
-      <h1>React Hooks - useReducer</h1>
+      <h1>React Unique ID</h1>
       <hr />
-      <p>Player 1: {state.player1}</p>
-      <p>Player 2: {state.player2}</p>
-      <button
-        onClick={() => {
-          dispacth({ type: 'SCORE1' })
+      <input
+        type="text"
+        value={nome}
+        onChange={e => {
+          setNome(e.target.value)
         }}
-      >
-        Pontos 1
-      </button>
-      <button
-        onClick={() => {
-          dispacth({ type: 'SCORE2' })
-        }}
-      >
-        Pontos 2
-      </button>
-      <button
-        onClick={() => {
-          dispacth({ type: 'RESETAR' })
-        }}
-      >
-        Resetar score
-      </button>
+      />
+      <button onClick={guardarNome}>Guardar Nome</button>
+      <hr />
+      <Nomes nomes={nomes} />
     </>
   )
 }
